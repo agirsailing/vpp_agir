02b6963ca0bb3d89822df29fc8ec5b7592a2aeec
kf5131 <kf5131@icloud.com>
2026-09-22 14:48:11 +0200

    Change values

--- hull.m
+++ hull.m
@@ -5,8 +5,8 @@ moth = get_boat_params(); % boat parameters
 
 % Boat parameters
 boat.hull.LWL   = moth.L_wl;      % waterline length [m]
-boat.hull.Swet  = 1;      % wetted surface area [m^2]
-boat.hull.disp  = 130;      % displacement [kg] - intial estimate
+boat.hull.Swet  = NaN;      % wetted surface area [m^2]
+boat.hull.mass  = moth.m_tot;      % mass [kg]
 
 
 
