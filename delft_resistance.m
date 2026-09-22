cf714b81a0848a2b723cd36c8d83ac3aea45d809
kf5131 <kf5131@icloud.com>
2026-09-22 14:27:07 +0200

    Add friction calculations

--- delft_resistance.m
+++ delft_resistance.m
@@ -17,6 +17,21 @@ L = boat.hull.LWL; % [m] waterline length
 S = boat.hull.Swet; % [m^2] wetted surface area
 
 
+% Froude number
+Fn = V / sqrt(g*L);
+
+% Friction
+Re = V * L / nu; % Reynolds number
+Cf = 0.075 / (log10(Re) - 2)^2;
+
+Rf = 0.5 * rho * V^2 * S * Cf;
+
+% Delft residuary resistance
+%Rr = delftResistance(boat, Fn);
+
+Rr = 0;
+
+R = Rf + Rr;
 
 
 
