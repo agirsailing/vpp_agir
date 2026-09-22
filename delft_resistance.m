47b09f9fa3a89be520aca510b9b4b14a9bdb95f5
kf5131 <kf5131@icloud.com>
2026-09-22 12:59:36 +0200

    Define inputs

---
+++ delft_resistance.m
@@ -0,0 +1,23 @@
+function delft_resistance(env, boat, moth, V)
+% delft_resistance - Calculate the resistance of a hull using Delft method
+
+%   Formula used:
+% R_hull(V) = R_F(V) + R_R,Delft(V)_; where V is velocity
+
+
+%   Inputs:
+
+%  environmentals
+rho = env.rho; % [kg/m^3] water density
+nu = env.water.nu;   % [m^2/s] kinematic viscosity
+g = env.water.g;     % [m/s^2] gravity acceleration
+
+% boat parameters
+L = boat.hull.LWL; % [m] waterline length
+S = boat.hull.Swet; % [m^2] wetted surface area
+
+
+
+
+
+end
